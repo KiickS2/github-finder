e5495858107835cd106668f321b01c790e47dec6
KiickS2 <jvmasz1@gmail.com>
2024-03-20 14:00:30 -0300

    Created error component

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -4,15 +4,27 @@ import User from "../components/User";
 
 import Search from "../components/Search";
 
+import Error from "../components/Error";
+
 import { useState } from "react";
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState<Boolean>(false);
 
   const loadUser = async (userName: string) => {
+
+    setError(false);
+
     const res = await fetch(`https://api.github.com/users/${userName}`);
 
     const data = await res.json();
+
+    if (res.status === 404) {
+      setError(true);
+      setUser(null);
+      return;
+    }
 
     const { avatar_url, login, location, followers, following } = data;
 
@@ -31,6 +43,7 @@ const Home = () => {
     <div>
       <Search loadUser={loadUser} />
       {user && <User {...user} />}
+      {error && <Error />}
     </div>
   );
 };
